a468cbeb47eac2a98635eae9e044df13c276a54c
funadmin <994927909@qq.com>
2021-12-16 21:23:18 +0800

    Update helper.php
    修复插件控制器大写的问题

--- src/helper.php
+++ src/helper.php
@@ -265,7 +265,8 @@ if (!function_exists('addons_url')) {
             $addons = count($route) == 3 ? strtolower($route[0]) : $request->addon;
             $controller = (array_pop($route)) ?: $request->param('controller');
             $module = (array_pop($route)) ?: $request->param('module', 'frontend');
-            $controller = Str::studly((string)$controller);
+            $module = lcfirst($module);
+            $controller = lcfirst(Str::studly((string)$controller));
             /* 解析URL带的参数 */
             if (isset($url['query'])) {
                 parse_str($url['query'], $query);
