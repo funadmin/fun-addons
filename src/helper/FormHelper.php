93230e610c3e02d93215fcf97ae3e343ede2f040
funadmin <994927909@qq.com>
2022-03-11 20:29:43 +0800

    Update FormHelper.php

--- src/helper/FormHelper.php
+++ src/helper/FormHelper.php
@@ -479,6 +479,7 @@ class FormHelper
         isset($options['create']) ? $op .= " data-create='" . $options['create'] . "'" : '';
         isset($options['theme']) ? $op .= " data-theme='" . $options['theme'] . "'" : '';
         isset($options['value']) ? $op .= " data-value='" . $options['value'] . "'" : '';
+        isset($options['toolbar']) ? $op .= " data-toolbar='" . json_encode($options['toolbar'],JSON_UNESCAPED_UNICODE) . "'" : '';
         $label = $options['label'] ?? $name;
         $str = '<div class="layui-form-item">
                 <label class="layui-form-label ' . self::labelRequire($options) . '">' . lang(Str::title($label)) . '</label>
