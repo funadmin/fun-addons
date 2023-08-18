0bb6931cf48cb328ab1fd96f702785ced030e429
funadmin <994927909@qq.com>
2023-08-18 09:33:24 +0800

    Add files via upload

--- src/helper/FormHelper.php
+++ src/helper/FormHelper.php
@@ -718,12 +718,14 @@ EOF;
 
     public function autocomplete($name='',$list=[],$options=[],$attr=[],$value=''){
         list($name,$id)= $this->getNameId($name,$options);
+        $disorread = $this->readonlyOrdisabled($options);
         $options['filter'] = 'autoComplete';
         $data = json_encode($list);
+        $attr = json_encode($attr);
         $str = <<<EOF
 <div class="layui-form-item ">{$this->label($name, $options)}
     <div class="layui-input-block">
-     <input data-data='{$data}'  type="search" dir="ltr" spellcheck=false autocorrect="off" autocomplete="off" autocapitalize="off" maxlength="2048" tabindex="1"
+     <input data-data='{$data}' data-attr="{$attr}" type="search" dir="ltr" spellcheck=false autocorrect="off" autocomplete="off" autocapitalize="off" maxlength="2048" tabindex="1"
         {$this->getDataPropAttr($name,$value,$options)} 
       {$this->getStyle($options)}  class="layui-input  {$this->getClass($options)}  $disorread "/>
      {$this->tips($options)} 
@@ -1081,7 +1083,7 @@ EOF;
                 'width' => $options['saveW'] ?? '300',
                 'height' => $options['saveW'] ?? '300',
                 'mark' => $options['mark'] ?? 1,
-                'area' => $options['area'] ?? '800px',
+                'area' => $options['area'] ?json_encode($options['area']): json_encode(['100%','100%']),
                 'filter' => 'cropper',
             ];
             $data_value = $this->getOptionsAttr($name,$cops);
@@ -1283,7 +1285,7 @@ EOF;
      * @return string
      */
     public function js($name=[],$options=[]){
-        if(is_string){
+        if(is_string($name)){
             $name = explode(',',$name);
         }
         $str = '';
@@ -1302,7 +1304,7 @@ EOF;
      * @return string
      */
     public function css($name=[],$options=[]){
-        if(is_string){
+        if(is_string($name)){
             $name = explode(',',$name);
         }
         $str = '';
@@ -1538,24 +1540,24 @@ EOF;
                         $attr.= $this->search($options);
                         break;
                     case 'value':
-                        $attr .=  $key.'="{$this->entities($val)}" data-'.$key.'="'.$this->entities($val).'" ';
+                        $attr .=  $key."='".$this->entities($val) ."' data-".$key."='".$this->entities($val)."' ";
                         break;
                     case 'attr':
                         if(is_object($val) || is_array($val)){
                             $val = (array)$val;
                             $val = implode(',',$val);
                         }
-                        $attr .= ' data-'.$key.'="'.$val.'" ';
+                        $attr .= " data-".$key."='".$val."' ";
                         break;
                     case 'skin':
-                        $attr.= "lay-'".$key.'"="'. $val.'" ';
+                        $attr.= " lay-'".$key.'"="'. $val.'" ';
                         break;
                     default:
                         if(is_object($val) || is_array($val)){
                             $val = (array)$val;
                             $val = $val[$name]?? json_encode($val, JSON_UNESCAPED_UNICODE);
                         }
-                        $attr .= ' data-'.$key.'="'.$val.'" ';
+                        $attr .= " data-".$key."='".$val."' ";
                         break;
 
                 }
